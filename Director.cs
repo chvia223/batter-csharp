02aa396f15241c2f629ef18c7fbd9bc22904efee
Christian Viazzo <via17001@byui.edu>
2021-11-20 22:04:27 -0700

    added win/lose conditions

--- Director.cs
+++ Director.cs
@@ -15,6 +15,7 @@ namespace cse210_batter_csharp
     /// </summary>
     public class Director
     {
+        private AudioService _audioService = new AudioService();
         private bool _keepPlaying = true;
         private Dictionary<string, List<Actor>> _cast;
         private Dictionary<string, List<Action>> _script;
@@ -36,13 +37,29 @@ namespace cse210_batter_csharp
                 CueAction("update");
                 CueAction("output");
 
+                if (_cast["balls"].Count == 0)
+                {
+                    _audioService.PlaySound(Constants.SOUND_OVER);
+                    _keepPlaying = false;
+                    Console.WriteLine("Game over!");
+                }
+
+                if (_cast["bricks"].Count == 0)
+                {
+                    _audioService.PlaySound(Constants.SOUND_OVER);
+                    _keepPlaying = false;
+                    Console.WriteLine("You win!");
+                }
+
                 if (Raylib_cs.Raylib.WindowShouldClose())
                 {
+                    _audioService.PlaySound(Constants.SOUND_OVER);
                     _keepPlaying = false;
+                    Console.WriteLine("Game over!");
                 }
             }
 
-            Console.WriteLine("Game over!");
+            
         }
 
         /// <summary>
